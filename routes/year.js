3fd665b457962f1c292e73e86090f89a014b0a48
David <david.pires2906@gmail.com>
2024-04-12 15:51:15 +0200

    vercel.json crée

--- routes/year.js
+++ routes/year.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/year', (req, res) => {
-    const actualYear = new Date().getFullYear.toString();
+router.get('/', (req, res) => {
+    const actualYear = new Date().getFullYear().toString();
     res.json( {year : actualYear})
 })
 
