22ead5c1f7d89c019d2605681b1fd760c0f79c6f
David <david.pires2906@gmail.com>
2024-04-12 15:45:04 +0200

    vercel.json crée

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var yearRouter = require('./routes/year');
+var yearRouter = require('./routes/year');
 
 var app = express();
 const cors = require('cors');
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/year', yearRouter);
+app.use('/year', yearRouter);
 
 module.exports = app;
